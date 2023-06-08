fb0330fb5f5b7981c9a6914155133d4b14dad1a7
Vincent Andrae <andrae.vincent@gmail.com>
2023-06-08 18:07:53 +0200

    Add sky and grass

--- src/Scenes/main.gd
+++ src/Scenes/main.gd
@@ -4,7 +4,10 @@ var rock_scene = preload("res://Scenes/rock.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	for i in 5:
+		var instance = rock_scene.instantiate()
+		instance.position = Vector2(200.0 + 10*i , 200.0 + 100*i)
+		self.add_child(instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
