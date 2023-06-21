ce88039ad9615e3371d0009b9e664cb695c8890a
Vincent Andrae <andrae.vincent@gmail.com>
2023-06-21 16:57:24 +0200

    Feat: Cleanup obstacles on screen exit

--- src/Scripts/garbage.gd
+++ src/Scripts/garbage.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 var garbage_images = [
+	"res://Images/garbage/Icon1.png",
 	"res://Images/garbage/Icon2.png", 
 	"res://Images/garbage/Icon3.png", 
-	"res://Images/garbage/Icon4.png", 
-	"res://Images/garbage/Icon8.png", 
-	"res://Images/garbage/Icon46.png", 
+	"res://Images/garbage/Icon4.png",
+	"res://Images/garbage/Icon6.png",
+	"res://Images/garbage/Icon10.png",
+	"res://Images/garbage/Icon23.png",
 	"res://Images/garbage/Icon47.png", 
 	"res://Images/garbage/Icons_15.png", 
-	"res://Images/garbage/Icons_28.png", 
-	"res://Images/garbage/Icons_31.png"
+	"res://Images/garbage/Icons_28.png"
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -22,3 +23,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
--- src/Scripts/rock.gd
+++ src/Scripts/rock.gd
@@ -77,3 +77,6 @@ func _process(_delta):
 
 func _init():
 	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
