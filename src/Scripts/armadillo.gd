68c3a2760ede720cee753aea9b327e027107808f
Vincent Andrae <andrae.vincent@gmail.com>
2023-06-21 17:26:35 +0200

    Add armadillo scene

--- src/Scripts/armadillo.gd
+++ src/Scripts/armadillo.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.play()
+	$AnimatedSprite2D.play("wait")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
