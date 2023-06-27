56a72fca40b6483319de7ea2a689f1ccc8478f83
noah <noah.becker@stud.tu-darmstadt.de>
2023-06-27 17:52:21 +0200

    layes now despawn properly, though their hitboxes are still wrong

--- src/Scripts/Section.gd
+++ src/Scripts/Section.gd
@@ -2,11 +2,11 @@ extends Area2D
 
 @export var color = Color(0.0, 0.5, 0.5)
 @export var horizontal_size:float = 3000
-@export var vertical_size:float = 100
+@export var vertical_size:float = 1000
 var last_section
 var next_section
 var size:Vector2
-@export var position_y:float = 500
+@export var position_y:float = 0
 var position_x:float = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -24,10 +24,18 @@ func _process(delta):
 	pass
 	
 	
-	
+func random_color():
+	# Generate random color components
+	var red = randf_range(0, 1)
+	var green = randf_range(0, 1)
+	var blue = randf_range(0, 1)
+	var alpha = 1.0  # Set alpha to 1 for full opacity
+	# Create a new random color
+	var randomColor = Color(red, green, blue, alpha)
+	return randomColor
 func _draw():	
 	print($Shape.shape.size)
-	draw_rect(Rect2(Vector2(position_x, position_y), $Shape.shape.size), color)
+	draw_rect(Rect2(Vector2(position_x, position_y), $Shape.shape.size), random_color())
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
@@ -39,7 +47,7 @@ func create_section():
 	var section = load("res://Scenes/section.tscn").instantiate()
 	section.position_y = self.position_y + self.vertical_size
 	section.last_section = self
-	add_child(section)
+	get_tree().get_root().add_child(section)
 	
 
 func _on_body_exited(body):
